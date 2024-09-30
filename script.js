f1c269d611e041ceb1bb718579b89fe4964be321
HankaCela <hana-cela@email.cz>
2024-09-30 20:00:31 +0200

    pridavam nove soubory

---
+++ script.js
@@ -0,0 +1,20 @@
+
+const cenaListku = 12;
+const pocetNavstevniku = 174;
+const pocetPredstaveni = 15;
+
+const prijem = cenaListku * pocetNavstevniku * pocetPredstaveni;
+
+const slevaProStudenty = 0.65 * cenaListku; 
+const podilStudentu = 0.4; 
+const podilPlaticichPlnouCenu = 1 - podilStudentu; 
+
+const prijemPlnaCena = pocetNavstevniku * podilPlaticichPlnouCenu * cenaListku * pocetPredstaveni;
+const prijemSeSlevou = pocetNavstevniku * podilStudentu * slevaProStudenty * pocetPredstaveni;
+
+const celkovyPrijemSeSlevou = prijemPlnaCena + prijemSeSlevou;
+
+document.body.innerHTML += `
+    <h1>Výpočet příjmu divadla</h1>
+    <p>Měsíční příjem: ${prijem} €</p>
+    <p>Měsíční příjem se studentskou slevou: ${celkovyPrijemSeSlevou} €</p>`;
