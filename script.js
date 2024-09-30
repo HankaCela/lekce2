be99ba4530b599eead749b6369bb835cceaff674
HankaCela <hana-cela@email.cz>
2024-09-30 20:10:20 +0200

    uprava souboru

--- script.js
+++ script.js
@@ -22,3 +22,7 @@ document.body.innerHTML += `
 
 // Hod kostkou
     let hodKostkou = Math.floor(Math.random() * 6) + 1;
+    document.body.innerHTML = `
+            <h1>Hod kostkou</h1>
+            <p>Padlo číslo: ${hodKostkou}</p>
+        `;
